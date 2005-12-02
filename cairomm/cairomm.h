6a464c2da1a0299886af28782dff759459b8858a
Murray Cumming <murrayc@murrayc.com>
2005-12-02 14:05:17 +0000

    2005-12-02  Murray Cumming <murrayc@murrayc.com>
            * cairomm/cairomm.h: Put sensible stuff in here,
            instead of my copy/paste stuff from libxml++.
            * cairomm/context.cc:
            * cairomm/context.h:
            * cairomm/enums.h:
            * cairomm/exception.cc:
            * cairomm/exception.h:
            * cairomm/fontface.cc:
            * cairomm/fontface.h:
            * cairomm/fontoptions.cc:
            * cairomm/fontoptions.h:
            * cairomm/path.cc:
            * cairomm/path.h:
            * cairomm/pattern.cc:
            * cairomm/pattern.h:
            * cairomm/private.cc:
            * cairomm/private.h:
            * cairomm/surface.cc:
            * cairomm/surface.h: Add LGPL comment headings.

--- cairomm/cairomm.h
+++ cairomm/cairomm.h
@@ -1,25 +1,28 @@
-/* node.h
- * libxml++ and this file are copyright (C) 2000 by Ari Johnson, and
- * are covered by the GNU Lesser General Public License, which should be
- * included with libxml++ as the file COPYING.
+/* Copyright (C) 2005 The cairomm Development Team
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free
+ * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
-#ifndef __LIBXMLCPP_H
-#define __LIBXMLCPP_H
+#ifndef __CAIROMM_H
+#define __CAIROMM_H
 
-#include <libxml++/exceptions/internal_error.h>
-#include <libxml++/exceptions/parse_error.h>
-#include <libxml++/parsers/domparser.h>
-#include <libxml++/parsers/saxparser.h>
-#include <libxml++/parsers/textreader.h>
-#include <libxml++/nodes/node.h>
-#include <libxml++/nodes/commentnode.h>
-#include <libxml++/nodes/element.h>
-#include <libxml++/nodes/entityreference.h>
-#include <libxml++/nodes/textnode.h>
-#include <libxml++/attribute.h>
-#include <libxml++/document.h>
-#include <libxml++/validators/validator.h>
-#include <libxml++/validators/dtdvalidator.h>
+#include <cairomm/context.h>
+#include <cairomm/surface.h>
+#include <cairomm/fontoptions.h>
+#include <cairomm/pattern.h>
+#include <cairomm/path.h>
+#include <cairomm/enums.h>
 
-#endif //__LIBXMLCPP_H
+#endif //__CAIROMM_H
