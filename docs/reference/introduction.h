1b1cc575b66902fef7887ef3539ec3afc83011ff
Jonathon Jongsma <jjongsma@gnome.org>
2006-02-08 00:55:17 +0000

    2006-02-07  Jonathon Jongsma  <jonathon.jongsma@gmail.com>
    	* Makefile.am: add docs/ subdir
    	* configure.in: added an --enable-docs switch to the configure script
    	(enabled by default), and added AC_OUTPUT directives for the documentation
    	Makefiles, etc.
    	* docs/.cvsignore:
    	* docs/Makefile.am:
    	* docs/reference/.cvsignore:
    	* docs/reference/Doxyfile.in:
    	* docs/reference/Makefile.am:
    	* docs/reference/introduction.h: Added infrastructure to build and install
    	the API documentation for cairomm (based on libxml++ makefiles).

---
+++ docs/reference/introduction.h
@@ -0,0 +1,11 @@
+/** \mainpage Cairomm: A C++ wrapper for the cairo graphics library
+ *
+ * \section License
+ * Cairomm is available under the terms of the LGPL license
+ *
+ * \section Introduction
+ * If you're just beginning to learn cairomm, a good place to start is with the
+ * Cairo::Surface and Cairo::Context classes.  In general terms, you draw onto
+ * a Surface using the graphics settings specified in your Context.
+ *
+ */
