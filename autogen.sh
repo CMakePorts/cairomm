4656575fcbccce2b09136fd7c123a90d9b4f5c3a
Jonathon Jongsma <jjongsma@gnome.org>
2006-02-21 05:20:05 +0000

    2006-02-17  Danilo Piazzalunga  <danilopiazza@gmail.com>
    	* COPYING: Use the text from the Library GPL 2.0, which is the actual
    	license of cairomm. Fixes Bug #5934
    	* cairomm/*: Update FSF's postal address in GPL/LGPL
    	comment headings.  Fixes Bug #5933

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-# $Id: autogen.sh,v 1.1 2005-12-02 13:34:41 murrayc Exp $
+# $Id: autogen.sh,v 1.2 2006-02-21 05:20:05 jjongsma Exp $
 #
 # Copyright (c) 2002  Daniel Elstner  <daniel.elstner@gmx.net>
 #
@@ -17,7 +17,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
 
 
 dir=`echo "$0" | sed 's,[^/]*$,,'`
