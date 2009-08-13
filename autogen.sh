cbed810a9b06b1ea4a89df4ec448e39ab7280682
Daniel Elstner <danielk@openismus.com>
2009-08-13 15:39:02 +0200

    Review and clean up after build overhaul
    * autogen.sh: Pass --enable-maintainer-mode to ./configure since the
    automatic rebuild of the reference documentation is only enabled in
    maintainer mode.  AM_MAINTAINER_MODE is already in configure.ac.
    * configure.ac (AC_CONFIG_HEADERS): Prepend build/config.h to the
    list of header files, because the first file in the list has its .in
    file generated by autoheader, and will thus include every AC_DEFINE
    from every Autoconf macro that is used.  The macros defined in the
    installed cairommconfig.h header should be namespaced and limited to
    meta information about the installed cairomm library.
    Also move cairommconfig.h to the top-level directory, in order to
    avoid the need to add the cairomm/ subdirectory to the include path.
    (PKG_CHECK_MODULES): Collapse the checks for optional cairo modules
    into a loop, and use PKG_CHECK_EXISTS() instead of the full-blown
    PKG_CHECK_MODULES().  Also, be a bit cleverer about the lists of .pc
    files and module names generated along the way.
    (AC_CONFIG_FILES): List all potentially installed files literally,
    instead of creating the list of output files dynamically.  This is
    much simpler and also gets us free shipping.  Remove data/Makefile.
    * cairommconfig.h.in: Add file to repository, as it should not be
    autogenerated.  Of the content, keep only the CAIROMM_ namespaced
    macros.
    * cairomm/context*.cc: Remove cairomm/ prefix from cairommconfig.h
    include statements.  This was already wrong before, but moving the
    file around made it visible.
    * Makefile.am: Clean up a bit.
    (DIST_SUBDIRS): Have Automake figure it out automatically.
    (cairomm_include_HEADERS): Remove, as cairomm/cairomm.h is already
    installed in cairomm/Makefile.am.
    (nodist_cairomm_libinclude_HEADERS): Relocate cairommconfig.h to
    the top-level directory.
    (nodist_pkgconfig_DATA): Use $(CAIROMM_INSTALL_PC) substitution from
    configure.ac to install the appropriate pkg-config data files.
    * cairomm/Makefile.am: Rewrite without using compile-binding.am, as
    it is not really the appropriate tool for the cairomm job.
    * cairomm/filelist.am (cairomm_cc): Rename from $(files_extra_cc).
    (cairomm_public_h): Rename variable from $(files_extra_h) and remove
    cairommconfig.h from the list.
    (cairomm_private_h): Rename variable from $(files_extra_ph).
    * docs/Makefile.am (doc_input): Adjust variable names.
    (dist_noinst_DATA): Add reference/cairomm.css to the list.
    (pubdocbase): Define for completeness.
    (htmlrefpub): Correct documentation URL.
    * docs/Doxyfile.in: Strip trailing whitespace from every line.
    (STRIP_FROM_PATH), (STRIP_FROM_INC_PATH), (INCLUDE_PATH): Do not
    strip the cairomm/ subdirectory prefix from the displayed filenames.
    (EXCLUDE): Remove list of files to exclude, since the list of input
    files is specified explicitly with the new build organization.
    (EXPAND_AS_DEFINED): Expand version number macros, although at the
    moment they are probably not used anywhere in the public headers.
    * data/cairomm-*.pc.in: Use @PACKAGE_VERSION@ instead of @VERSION@.
    * data/cairomm-1.0.pc.in (htmlrefpub): Correct documentation URL.
    (Cflags): Add missing -I${libdir}/@CAIROMM_MODULE_NAME@/include.
    * data/Makefile.am: Delete now unused build file.
    * docs/reference/Makefile.am: Delete left-over build file.

--- autogen.sh
+++ autogen.sh
@@ -1,26 +1,7 @@
 #! /bin/sh -e
-
-# Copyright (c) 2009 Openismus GmbH
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License VERSION 2 as
-# published by the Free Software Foundation.  You are not allowed to
-# use any other version of the license; unless you got the explicit
-# permission from the author to do so.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-# 02110-1301, USA.
-
 test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
 
 mm-common-prepare --force --copy "$srcdir"
 autoreconf --force --install "$srcdir"
-test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
+test -n "$NOCONFIGURE" || "$srcdir/configure" --enable-maintainer-mode "$@"
